facdfa0a48fdafe0cbc05809f32a5e3f526b9d96
Tobias <74780616+Syriuus@users.noreply.github.com>
2024-02-27 10:35:19 +0100

    Added GetWinrateByRole

--- src/main/java/de/syriuus/Main.java
+++ src/main/java/de/syriuus/Main.java
@@ -5,6 +5,7 @@ import de.syriuus.utils.RiotAPIKey;
 
 public class Main {
     public static void main(String[] args) {
-        RestClient.getMatchData("EUW1_6784224177");
+        //_vxOMo-r1ELGfhsOcDMT5blJtLxpSkRBKVZ8gTcgIinYmoPZWY0TL-81i25dn0oWqrzBC-NaYA52SA
+        System.out.println(RestClient.getWinrateForRole("TOP", "_vxOMo-r1ELGfhsOcDMT5blJtLxpSkRBKVZ8gTcgIinYmoPZWY0TL-81i25dn0oWqrzBC-NaYA52SA", 10));
     }
 }
--- src/main/java/de/syriuus/rest/client/RestClient.java
+++ src/main/java/de/syriuus/rest/client/RestClient.java
@@ -9,28 +9,41 @@ import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.ArrayList;
 import java.util.List;
 
 public class RestClient {
-	private static String baseURL = "https://europe.api.riotgames.com";
+	private static String matchesURL = "https://europe.api.riotgames.com";
 
-	public static void getMatches(String puid, int count) {
+	private static List<MatchData> getMatches(String puid, int count) {
 		int start = 0;
-		String url = baseURL + "/lol/match/v5/matches/by-puuid/" + puid + "/ids?start=" + start + "&count=" + count;
+		List<MatchData> matchDataList = new ArrayList<>();
+
+		String url = matchesURL + "/lol/match/v5/matches/by-puuid/" + puid + "/ids?start=" + start + "&count=" + count;
 		System.out.println("Calling url=" + url);
 		Client client = ClientBuilder.newClient();
 		Response response = client.target(url).request(MediaType.APPLICATION_JSON).header("X-Riot-Token", RiotAPIKey.getInstance().getApiKey()).get();
-		List<String> user = response.readEntity(new GenericType<List<String>>() {});
-		user.forEach(System.out::println);
+		List<String> matchIDList = response.readEntity(new GenericType<List<String>>() {});
+		matchIDList.forEach(id -> {
+			matchDataList.add(getMatchData(id));
+		});
+		return matchDataList;
 	}
 
-	public static void getMatchData(String matchID){
-		String url = baseURL + "/lol/match/v5/matches/" + matchID;
+	private static MatchData getMatchData(String matchID){
+		String url = matchesURL + "/lol/match/v5/matches/" + matchID;
 		System.out.println("Calling url=" + url);
 		Client client = ClientBuilder.newClient();
 		Response response = client.target(url).request(MediaType.APPLICATION_JSON).header("X-Riot-Token", RiotAPIKey.getInstance().getApiKey()).get();
-		MatchData matchData = response.readEntity(MatchData.class);
+		return response.readEntity(MatchData.class);
+	}
 
+	public static double getWinrateForRole(String role, String puid, int count){
+		List<MatchData> matchDataList = getMatches(puid, count);
+
+		List<MatchData> test = matchDataList.stream().filter(data -> data.getInfo().getParticipants().stream().anyMatch(player -> player.getTeamPosition().equalsIgnoreCase(role) && player.getPuuid().equals(puid) && player.getWin())).toList();
+		// List<MatchData> test = matchDataList.stream().filter(data -> data.getInfo().getParticipants().stream().anyMatch(player -> player.getPuuid().equals(puid) && player.getWin())).toList();
+		return ((double)test.size()/(double)matchDataList.size())*100;
 	}
 
 
