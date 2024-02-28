b4a2ac0b46972eb148892fa28d690601a262f3bb
Tobias <74780616+Syriuus@users.noreply.github.com>
2024-02-28 17:58:01 +0100

    Method arguements made optional

--- src/main/java/de/syriuus/rest/client/RestClient.java
+++ src/main/java/de/syriuus/rest/client/RestClient.java
@@ -15,8 +15,17 @@ import java.util.List;
 public class RestClient {
 	private static String matchesURL = "https://europe.api.riotgames.com";
 
-	private static List<MatchData> getMatches(String puid, int count) {
-		int start = 0;
+	public static double getWinrateForRole(String role, String puid, int count, int start){
+		return getWinrateForRolePrivate(role, puid, count, start);
+	}
+	public static double getWinrateForRole(String role, String puid, int count){
+		return getWinrateForRolePrivate(role, puid, count, 0);
+	}
+	public static double getWinrateForRole(String role, String puid){
+		return getWinrateForRolePrivate(role, puid, 20, 0);
+	}
+
+	private static List<MatchData> getMatches(String puid, int count, int start) {
 		List<MatchData> matchDataList = new ArrayList<>();
 
 		String url = matchesURL + "/lol/match/v5/matches/by-puuid/" + puid + "/ids?start=" + start + "&count=" + count;
@@ -38,10 +47,15 @@ public class RestClient {
 		return response.readEntity(MatchData.class);
 	}
 
-	public static double getWinrateForRole(String role, String puid, int count){
-		List<MatchData> matchDataList = getMatches(puid, count);
+	private static double getWinrateForRolePrivate(String role, String puid, int count, int start){
+		List<MatchData> matchDataList = getMatches(puid, count, start);
 
-		List<MatchData> test = matchDataList.stream().filter(data -> data.getInfo().getParticipants().stream().anyMatch(player -> player.getTeamPosition().equalsIgnoreCase(role) && player.getPuuid().equals(puid) && player.getWin())).toList();
+		List<MatchData> test = matchDataList.stream().filter(data ->
+				data.getInfo().getParticipants()
+						.stream()
+						.anyMatch(player ->
+								player.getTeamPosition().equalsIgnoreCase(role) && player.getPuuid().equals(puid) && player.getWin()))
+				.toList();
 		// List<MatchData> test = matchDataList.stream().filter(data -> data.getInfo().getParticipants().stream().anyMatch(player -> player.getPuuid().equals(puid) && player.getWin())).toList();
 		return ((double)test.size()/(double)matchDataList.size())*100;
 	}
